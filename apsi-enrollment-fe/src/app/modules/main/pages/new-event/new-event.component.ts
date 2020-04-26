a67e424b03aa31f02df1db84efd0f8c58c37fa6f
mswianiewicz <maria.swianiewicz@comp.com.pl>
2020-04-26 17:43:59 +0200

    Added form time validation and place select

--- apsi-enrollment-fe/src/app/modules/main/pages/new-event/new-event.component.ts
+++ apsi-enrollment-fe/src/app/modules/main/pages/new-event/new-event.component.ts
@@ -1,5 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators} from '@angular/forms';
+import {EventType} from '../../../../core/model/event.model';
+
+export const timeValidator: ValidatorFn = (formGroup: FormGroup): ValidationErrors | null => {
+  const startDate = formGroup.get('startDate').value;
+  const startTime = formGroup.get('startTime').value;
+  const endDate = formGroup.get('endDate').value;
+  const endTime = formGroup.get('endTime').value;
+  if (startDate && startTime && endDate && endTime) {
+    const start = parseDate(startDate, startTime);
+    const end = parseDate(endDate, endTime);
+    if (start > end) {
+      return {
+        time: true
+      };
+    }
+  }
+};
+
+function parseDate(date: string, time: string) {
+  const d = new Date(date);
+  const splitTime = time.split(':');
+  d.setHours(Number(splitTime[0]));
+  d.setMinutes(Number(splitTime[1]));
+  return d;
+}
 
 @Component({
   selector: 'app-new-event',
@@ -8,6 +33,7 @@ import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class NewEventComponent implements OnInit {
   eventForm: FormGroup;
+  eventTypes = EventType;
 
   constructor(private fb: FormBuilder) {
     this.eventForm = fb.group({
@@ -25,14 +51,17 @@ export class NewEventComponent implements OnInit {
 
   addMeeting() {
     const meetingGroup = this.fb.group({
-      description: [null],
-      startDate: [null, [Validators.required]],
-      startTime: [null, [Validators.required]],
-      endDate: [null, [Validators.required]],
-      endTime: [null, [Validators.required]],
-      speakers: this.fb.array([]),
-      place: [null, [Validators.required]],
-    });
+        description: [null],
+        startDate: [null, [Validators.required]],
+        startTime: [null, [Validators.required]],
+        endDate: [null, [Validators.required]],
+        endTime: [null, [Validators.required]],
+        speakers: this.fb.array([]),
+        place: [null, [Validators.required]],
+      },
+      {
+        validator: timeValidator,
+      });
     this.getMeetings().push(meetingGroup);
   }
 
@@ -52,7 +81,9 @@ export class NewEventComponent implements OnInit {
     );
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
 
-  submit() {}
+  submit() {
+  }
 }
