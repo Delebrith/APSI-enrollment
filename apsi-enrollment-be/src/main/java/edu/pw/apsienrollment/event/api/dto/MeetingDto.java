54b564ca05136c081c5fcf98cba697e20d4e1a0f
lychanl <lycha_@poczta.onet.pl>
2020-04-26 10:45:31 +0200

    GET events/{id} now returns also a list of meetings

---
+++ apsi-enrollment-be/src/main/java/edu/pw/apsienrollment/event/api/dto/MeetingDto.java
@@ -0,0 +1,35 @@
+package edu.pw.apsienrollment.event.api.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.constraints.NotNull;
+
+import edu.pw.apsienrollment.event.db.Meeting;
+import edu.pw.apsienrollment.place.api.PlaceDto;
+import edu.pw.apsienrollment.user.api.UserDto;
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder
+public class MeetingDto {
+    @NotNull Long id;
+    String desctiption;
+    @NotNull LocalDateTime start;
+    @NotNull LocalDateTime end;
+    @NotNull List<UserDto> speakers;
+    @NotNull PlaceDto place;
+
+    public static MeetingDto of(Meeting meeting) {
+        return builder()
+            .id(meeting.getId())
+            .desctiption(meeting.getDescription())
+            .start(meeting.getStart())
+            .end(meeting.getEnd())
+            .speakers(meeting.getSpeakers().stream().map(UserDto::of).collect(Collectors.toList()))
+            .place(PlaceDto.of(meeting.getPlace()))
+            .build();
+    }
+}
