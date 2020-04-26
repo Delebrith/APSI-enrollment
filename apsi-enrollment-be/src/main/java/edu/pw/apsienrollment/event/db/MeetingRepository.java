d913576c0adbb0b9a576660a94c671d7d81cfc88
lychanl <lycha_@poczta.onet.pl>
2020-04-26 10:45:31 +0200

    GET events/{id} now returns also a list of meetings

--- apsi-enrollment-be/src/main/java/edu/pw/apsienrollment/event/db/MeetingRepository.java
+++ apsi-enrollment-be/src/main/java/edu/pw/apsienrollment/event/db/MeetingRepository.java
@@ -1,8 +1,11 @@
 package edu.pw.apsienrollment.event.db;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MeetingRepository extends JpaRepository<Meeting, Long> {
+    List<Meeting> findByEvent(Event event);
 }
