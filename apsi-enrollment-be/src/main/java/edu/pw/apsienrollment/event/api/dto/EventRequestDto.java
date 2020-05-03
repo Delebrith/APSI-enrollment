a4a5d127f8c023ea11786100a1e10f03737109c9
Delebrith <paulina.szwed.1996@gmail.com>
2020-05-03 10:45:05 +0200

    add validation on EventRequestDto.cost

--- apsi-enrollment-be/src/main/java/edu/pw/apsienrollment/event/api/dto/EventRequestDto.java
+++ apsi-enrollment-be/src/main/java/edu/pw/apsienrollment/event/api/dto/EventRequestDto.java
@@ -3,10 +3,7 @@ package edu.pw.apsienrollment.event.api.dto;
 import edu.pw.apsienrollment.event.db.EventType;
 import lombok.Value;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.*;
 import java.util.Set;
 
 @Value
@@ -21,5 +18,5 @@ public class EventRequestDto {
 
     @NotEmpty Set<MeetingRequestDto> meetings;
 
-    Float cost = 0f;
+    @PositiveOrZero Float cost = 0f;
 }
