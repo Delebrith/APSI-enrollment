06c6d4898b65621cb47aec75458f9c740ec8fc9c
Wojciech Gruszka <wojciech@gruszka.eu>
2020-05-17 20:23:53 +0200

    Api exception frontend integration

--- apsi-enrollment-fe/src/app/core/model/api-error.model.ts
+++ apsi-enrollment-fe/src/app/core/model/api-error.model.ts
@@ -1,10 +1,21 @@
 export enum APIErrorMessageType {
-  UNKNOWN = 'unknown',
+  UNKNOWN = 'UNKNOWN',
+  INVALID_CREDENTIALS = 'INVALID_CREDENTIALS',
+  REFRESH_TOKEN_NOT_VALID = 'REFRESH_TOKEN_NOT_VALID',
+  REFRESH_TOKEN_NOT_FOUND = 'REFRESH_TOKEN_NOT_FOUND',
+  EVENT_NOT_FOUND = 'EVENT_NOT_FOUND',
+  UNAUTHORIZED_TO_CREATE_EVENT = 'UNAUTHORIZED_TO_CREATE_EVENT',
+  PLACE_TOO_SMALL = 'PLACE_TOO_SMALL',
+  PLACE_NOT_AVAILABLE = 'PLACE_NOT_AVAILABLE',
+  SPEAKER_UNAVAILABLE = 'SPEAKER_UNAVAILABLE',
+  PLACE_NOT_FOUND = 'PLACE_NOT_FOUND',
+  VALIDATION_FAILED = 'VALIDATION_FAILED',
+  USER_NOT_FOUND = 'USER_NOT_FOUND',
 }
 
 export interface APIError {
   status: number;
   message: string;
-  messageKey: APIErrorMessageType;
-  messageParams: { [key: string]: any };
+  code: APIErrorMessageType;
+  params: { [key: string]: any };
 }
