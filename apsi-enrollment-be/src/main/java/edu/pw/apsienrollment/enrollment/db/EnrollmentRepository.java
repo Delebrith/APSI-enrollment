935a96ada1acac96d5f78d34e0c36e52cc4cd94c
Delebrith <paulina.szwed.1996@gmail.com>
2020-05-18 22:04:25 +0200

    add endpoint for users enrollments with status

--- apsi-enrollment-be/src/main/java/edu/pw/apsienrollment/enrollment/db/EnrollmentRepository.java
+++ apsi-enrollment-be/src/main/java/edu/pw/apsienrollment/enrollment/db/EnrollmentRepository.java
@@ -1,16 +1,17 @@
 package edu.pw.apsienrollment.enrollment.db;
 
-import java.util.List;
-
+import edu.pw.apsienrollment.event.db.Event;
+import edu.pw.apsienrollment.user.db.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import edu.pw.apsienrollment.event.db.Event;
-import edu.pw.apsienrollment.user.db.User;
+import java.util.Collection;
 
 @Repository
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
     Long countByEvent(Event event);
 
     boolean existsByUserAndEvent(User user, Event event);
+
+    Collection<Enrollment> findByUser(User user);
 }
