99a0f960ae55f8fda621c14ca08cb36f72a46438
Delebrith <paulina.szwed.1996@gmail.com>
2020-05-18 22:04:25 +0200

    add endpoint for users enrollments with status

--- apsi-enrollment-be/src/main/java/edu/pw/apsienrollment/enrollment/api/EnrollmentController.java
+++ apsi-enrollment-be/src/main/java/edu/pw/apsienrollment/enrollment/api/EnrollmentController.java
@@ -1,8 +1,8 @@
 package edu.pw.apsienrollment.enrollment.api;
 
 import edu.pw.apsienrollment.enrollment.EnrollmentService;
-import edu.pw.apsienrollment.enrollment.api.dto.EnrollmentRequestDto;
 import edu.pw.apsienrollment.enrollment.api.dto.EnrollmentDto;
+import edu.pw.apsienrollment.enrollment.api.dto.EnrollmentRequestDto;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
@@ -13,7 +13,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.net.URI;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("enrollment")
@@ -31,5 +31,19 @@ public class EnrollmentController {
     ResponseEntity<EnrollmentDto> signUp(@Valid @RequestBody EnrollmentRequestDto enrollmentRequestDto) {
         EnrollmentDto enrollment = EnrollmentDto.of(enrollmentService.signUp(enrollmentRequestDto.getEventId()));
         return ResponseEntity.ok(enrollment);
+    }
+
+    @ApiOperation(value = "Get my enrollments", nickname = "my enrollments", notes="",
+            authorizations = {@Authorization(value = "JWT")})
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "If valid credentials were provided", response = Iterable.class),
+            @ApiResponse(code = 400, message = "If invalid data was provided")})
+    @GetMapping("my-enrollments")
+    ResponseEntity<Iterable<EnrollmentDto>> getMyEnrollments() {
+        Iterable<EnrollmentDto> enrollments = enrollmentService.getMyEnrollments()
+                .stream()
+                .map(EnrollmentDto::of)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(enrollments);
     }
 }
