049fb702c42fd116c7d5325987990a46cea57586
lychanl <lycha_@poczta.onet.pl>
2020-05-23 18:57:02 +0200

    Private fields

--- apsi-enrollment-be/src/main/java/edu/pw/apsienrollment/payment/api/dto/CurrencyDto.java
+++ apsi-enrollment-be/src/main/java/edu/pw/apsienrollment/payment/api/dto/CurrencyDto.java
@@ -1,10 +1,8 @@
 package edu.pw.apsienrollment.payment.api.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Value;
 
 @Value
-@AllArgsConstructor
 public class CurrencyDto {
     private String currency;
 }
