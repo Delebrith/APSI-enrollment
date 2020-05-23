e8ba660a4cf319d11bc32d2c1ec94ab8729173af
lychanl <lycha_@poczta.onet.pl>
2020-05-23 18:57:02 +0200

    Private fields

--- apsi-enrollment-be/src/main/java/edu/pw/apsienrollment/payment/api/dto/PaymentCreateDto.java
+++ apsi-enrollment-be/src/main/java/edu/pw/apsienrollment/payment/api/dto/PaymentCreateDto.java
@@ -10,5 +10,5 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PaymentCreateDto {
-    @NotNull Long enrollmentId;
+    private @NotNull Long enrollmentId;
 }
