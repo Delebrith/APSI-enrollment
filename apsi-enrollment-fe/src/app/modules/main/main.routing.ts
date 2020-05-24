e92c84287a14d7cf730a3e9d171c365d8960a1ec
gala377 <r.galczak@gmail.com>
2020-05-24 21:08:07 +0000

    FE: Review changes

--- apsi-enrollment-fe/src/app/modules/main/main.routing.ts
+++ apsi-enrollment-fe/src/app/modules/main/main.routing.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main.component';
 import { AllEventsComponent } from './pages/all-events/all-events.component';
+import { AllPaymentsComponent } from './pages/all-payments/all-payments.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NewEventComponent } from './pages/new-event/new-event.component';
-import { AllPaymentsComponent } from './pages/all-payments/all-payments.component';
 
 const routes: Routes = [
   {
