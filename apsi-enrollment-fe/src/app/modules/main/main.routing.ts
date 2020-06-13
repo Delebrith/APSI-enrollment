01e11d068a19c35728543e56d4d67c1133f228d9
lychanl <lycha_@poczta.onet.pl>
2020-06-14 01:26:48 +0200

    My events view

--- apsi-enrollment-fe/src/app/modules/main/main.routing.ts
+++ apsi-enrollment-fe/src/app/modules/main/main.routing.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main.component';
 import { AllEventsComponent } from './pages/all-events/all-events.component';
+import { MyEventsComponent } from './pages/my-events/my-events.component';
 import { AllPaymentsComponent } from './pages/all-payments/all-payments.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NewEventComponent } from './pages/new-event/new-event.component';
@@ -18,6 +19,10 @@ const routes: Routes = [
       {
         path: 'all-events',
         component: AllEventsComponent,
+      },
+      {
+        path: 'my-events',
+        component: MyEventsComponent,
       },
       {
         path: 'new-event',
