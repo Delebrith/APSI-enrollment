6ebe52f582d087d01d4a35c807b6e81f8cc07f20
lychanl <lycha_@poczta.onet.pl>
2020-06-20 16:00:36 +0200

    Fixes

--- apsi-enrollment-be/src/main/java/edu/pw/apsienrollment/attendance/api/AttendanceController.java
+++ apsi-enrollment-be/src/main/java/edu/pw/apsienrollment/attendance/api/AttendanceController.java
@@ -2,16 +2,19 @@ package edu.pw.apsienrollment.attendance.api;
 
 import javax.validation.Valid;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.pw.apsienrollment.attendance.AttendanceService;
 import edu.pw.apsienrollment.attendance.api.dto.AttendanceDto;
 import edu.pw.apsienrollment.common.api.dto.PageRequestDTO;
-import edu.pw.apsienrollment.event.api.dto.MeetingDto;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponses;
 import io.swagger.annotations.ApiResponse;
@@ -36,4 +39,29 @@ public class AttendanceController {
         return ResponseEntity.ok(attendanceService.getMeetingsOfAuthorizedUser(request.getPage(), request.getSize())
             .map(AttendanceDto::of));
     }
+
+    
+    @ApiOperation(value = "Get attendance QR code", nickname = "get attendance QR code", notes = "",
+            authorizations = {@Authorization(value = "JWT")})
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "If valid credentials were provided", response = Iterable.class),
+            @ApiResponse(code = 400, message = "If invalid data was provided")})
+    @GetMapping(value = "{id}/qr-code", produces = MediaType.IMAGE_PNG_VALUE)
+    ResponseEntity<byte[]> getQRCode(@PathVariable Long id) {
+        return ResponseEntity.ok(attendanceService.getQrCode(id));
+    }
+
+    @ApiOperation(value = "Check attendance QR code validity and mark user as present",
+            nickname = "check attendance QR code", notes = "",
+            authorizations = {@Authorization(value = "JWT")})
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "If valid credentials were provided", response = Iterable.class),
+            @ApiResponse(code = 400, message = "If invalid data was provided")})
+    @PostMapping("{id}/mark-as-present")
+    ResponseEntity<Void> markAsPresent(@PathVariable Long id,
+                                       @RequestParam String token) {
+        attendanceService.markAsPresent(id, token);
+        return ResponseEntity.ok().build();
+    }
+
 }
