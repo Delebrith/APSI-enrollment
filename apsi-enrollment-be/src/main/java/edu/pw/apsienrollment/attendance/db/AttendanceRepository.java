e635b3b1a8ed20597acf942aecc37eb7acd5b065
Delebrith <paulina.szwed.1996@gmail.com>
2020-06-20 13:08:25 +0200

    create dynamically started AttendanceCheckJob

--- apsi-enrollment-be/src/main/java/edu/pw/apsienrollment/attendance/db/AttendanceRepository.java
+++ apsi-enrollment-be/src/main/java/edu/pw/apsienrollment/attendance/db/AttendanceRepository.java
@@ -1,16 +1,14 @@
 package edu.pw.apsienrollment.attendance.db;
 
 import edu.pw.apsienrollment.event.meeting.Meeting;
-import org.springframework.data.domain.Pageable;
+import edu.pw.apsienrollment.user.db.User;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
-
-import edu.pw.apsienrollment.user.db.User;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
     Optional<Attendance> findByIdAndTokenAndMeeting_Id(Long id, String token, Long meetingId);
@@ -18,4 +16,8 @@ public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
     Page<Attendance> findByUser(User user, Pageable pageable);
 
     List<Attendance> findByMeetingOrderByUser_Surname(@Param("meeting") Meeting meeting);
+
+    boolean existsByMeeting_IdAndAttendanceStatus(Long meetingId, AttendanceStatus present);
+
+    List<Attendance> findByMeeting_IdAndAndAttendanceStatus(Long meetingId, AttendanceStatus unchecked);
 }
