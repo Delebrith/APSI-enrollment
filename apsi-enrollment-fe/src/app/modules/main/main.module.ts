81ffae39d4c9b9134e997a08f41b0589cf89baab
lychanl <lycha_@poczta.onet.pl>
2020-06-20 13:29:32 +0200

    Organized evets in my-events view

--- apsi-enrollment-fe/src/app/modules/main/main.module.ts
+++ apsi-enrollment-fe/src/app/modules/main/main.module.ts
@@ -9,11 +9,15 @@ import {AllEventsComponent} from './pages/all-events/all-events.component';
 import {EventDetailComponent} from './pages/event-detail/event-detail.component';
 import {HomeComponent} from './pages/home/home.component';
 import {NewEventComponent} from './pages/new-event/new-event.component';
-import { AllPaymentsComponent } from './pages/all-payments/all-payments.component';
-import { MyEventsComponent } from './pages/my-events/my-events.component';
+import {AllPaymentsComponent} from './pages/all-payments/all-payments.component';
+import {MyEventsComponent} from './pages/my-events/my-events.component';
+import {MySpeakerEventsComponent} from './pages/my-speaker-events/my-speaker-events.component';
+import {MyOrganizerEventsComponent} from './pages/my-organizer-events/my-organizer-events.component';
 
 @NgModule({
-  declarations: [HomeComponent, MainComponent, AllEventsComponent, MyEventsComponent, AllPaymentsComponent, EventDetailComponent, NewEventComponent],
+  declarations: [
+    HomeComponent, MainComponent, AllEventsComponent, MyEventsComponent, MySpeakerEventsComponent, MyOrganizerEventsComponent,
+    AllPaymentsComponent, EventDetailComponent, NewEventComponent],
   imports: [CommonModule, MainRoutingModule, CoreModule, SharedModule, ReactiveFormsModule],
 })
 export class MainModule {
