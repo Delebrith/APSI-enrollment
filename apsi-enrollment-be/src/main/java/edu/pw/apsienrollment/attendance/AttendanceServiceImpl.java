a527e6fa72e600a9c51dd50fdc621fcfa00c5518
Delebrith <paulina.szwed.1996@gmail.com>
2020-06-20 14:33:08 +0200

    change Attendance entity to include token for security purposes. Add attendance check endpoint by moving logic from EnrollmentService

--- apsi-enrollment-be/src/main/java/edu/pw/apsienrollment/attendance/AttendanceServiceImpl.java
+++ apsi-enrollment-be/src/main/java/edu/pw/apsienrollment/attendance/AttendanceServiceImpl.java
@@ -1,9 +1,14 @@
 package edu.pw.apsienrollment.attendance;
 
+import com.google.common.collect.ImmutableMap;
 import edu.pw.apsienrollment.attendance.db.Attendance;
 import edu.pw.apsienrollment.attendance.db.AttendanceRepository;
 import edu.pw.apsienrollment.attendance.db.AttendanceStatus;
+import edu.pw.apsienrollment.attendance.exception.AttendanceNotFoundException;
+import edu.pw.apsienrollment.attendance.exception.UserUnauthorizedToCheckAttendanceException;
+import edu.pw.apsienrollment.authentication.AuthenticationService;
 import edu.pw.apsienrollment.event.meeting.Meeting;
+import edu.pw.apsienrollment.qrcode.QRCodeService;
 import edu.pw.apsienrollment.user.db.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,6 +17,11 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AttendanceServiceImpl implements AttendanceService {
     private final AttendanceRepository attendanceRepository;
+    private final QRCodeService qrcodeService;
+    private final AuthenticationService authenticationService;
+
+    private static final String confirmURLScheme = "attendance/%d/mark-as-present?token=%s";
+    private static final Integer qrCodeImageSize = 640;
 
     @Override
     public Attendance putIntoAttendanceList(Meeting meeting, User user) {
@@ -21,5 +31,33 @@ public class AttendanceServiceImpl implements AttendanceService {
                 .attendanceStatus(AttendanceStatus.UNCHECKED)
                 .build();
         return attendanceRepository.save(attendance);
+    }
+
+    @Override
+    public byte[] getQrCode(Long attendanceId) {
+        Attendance attendance = attendanceRepository.findById(attendanceId)
+                .orElseThrow(AttendanceNotFoundException::new);
+        return qrcodeService.generateQRCode(
+                String.format(confirmURLScheme, attendance.getId(), attendance.getToken()),
+                qrCodeImageSize,
+                qrCodeImageSize);
+    }
+
+    @Override
+    public void markAsPresent(Long id, String token) {
+        Attendance attendance = attendanceRepository.findByIdAndToken(id, token)
+                .orElseThrow(() -> new AttendanceNotFoundException(
+                        "Attendance not found", ImmutableMap.of("id", id, "token", token)));
+        checkIfUserIsOrganizerOrSpeaker(attendance);
+        attendance.setAttendanceStatus(AttendanceStatus.PRESENT);
+        attendanceRepository.save(attendance);
+    }
+
+    private void checkIfUserIsOrganizerOrSpeaker(Attendance attendance) {
+        User authenticated = authenticationService.getAuthenticatedUser();
+        if (!attendance.getMeeting().getSpeakers().contains(authenticated)
+                && !attendance.getMeeting().getEvent().getOrganizer().equals(authenticated)) {
+            throw new UserUnauthorizedToCheckAttendanceException();
+        }
     }
 }
