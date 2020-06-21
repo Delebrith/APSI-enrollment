7d22c971be218697db003422c501efd83c21c031
Wojciech Gruszka <wojciech@gruszka.eu>
2020-06-21 14:12:56 +0200

    error handlers for all requests

--- apsi-enrollment-fe/src/app/modules/main/pages/all-events/all-events.component.ts
+++ apsi-enrollment-fe/src/app/modules/main/pages/all-events/all-events.component.ts
@@ -3,6 +3,7 @@ import { ClrDatagridStateInterface } from '@clr/angular';
 import { Observable } from 'rxjs';
 import { BasicEvent } from 'src/app/core/model/event.model';
 import { Page, PageRequest } from 'src/app/core/model/pagination.model';
+import { AlertService } from '../../services/alert/alert.service';
 import { EventService } from '../../services/event/event.service';
 import { EventDetailComponent } from '../event-detail/event-detail.component';
 
@@ -16,7 +17,7 @@ export class AllEventsComponent implements OnInit {
   totalEvents: number;
   loading = true;
 
-  constructor(private eventService: EventService) {}
+  constructor(private eventService: EventService, private alertService: AlertService) {}
 
   getEvents(request: PageRequest): Observable<Page<BasicEvent>> {
     return this.eventService.getEventsPage(request);
@@ -29,8 +30,7 @@ export class AllEventsComponent implements OnInit {
     let searchString: string = null;
 
     if (state.filters) {
-      searchString = state.filters.reduce(
-        (prev, next) => `${prev}${next.property}=${next.value},`, '');
+      searchString = state.filters.reduce((prev, next) => `${prev}${next.property}=${next.value},`, '');
       searchString = searchString.substring(0, searchString.length - 1);
     }
 
@@ -39,11 +39,19 @@ export class AllEventsComponent implements OnInit {
       pageSize: state.page.size,
       searchQuery: searchString,
     };
-    this.getEvents(request).subscribe((page) => {
+    this.getEvents(request).subscribe(
+      (page) => {
         this.events = page.items;
         this.totalEvents = page.totalElements;
         this.loading = false;
-      });
+      },
+      (error) => {
+        this.alertService.showError(error);
+        this.events = [];
+        this.totalEvents = 0;
+        this.loading = false;
+      }
+    );
   }
 
   onShowEventDetails(eventId: number) {
