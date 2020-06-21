37214947739e05d88710292fd846a7ce9633d97f
Wojciech Gruszka <wojciech@gruszka.eu>
2020-06-21 23:08:16 +0200

    Formatting

--- apsi-enrollment-fe/src/app/core/model/attendance.model.ts
+++ apsi-enrollment-fe/src/app/core/model/attendance.model.ts
@@ -1,13 +1,11 @@
 import { Meeting } from './event.model';
 import { User } from './user.model';
 
-
 export enum AttendanceStatus {
   UNCHECKED = 'UNCHECKED',
   PRESENT = 'PRESENT',
   ABSENT = 'ABSENT',
 }
-
 
 export interface Attendance {
   id: number;
--- apsi-enrollment-fe/src/app/modules/main/pages/my-events/my-events.component.ts
+++ apsi-enrollment-fe/src/app/modules/main/pages/my-events/my-events.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    templateUrl: './my-events.component.html',
-    styleUrls: ['./my-events.component.scss'],
-  })
+  templateUrl: './my-events.component.html',
+  styleUrls: ['./my-events.component.scss'],
+})
 export class MyEventsComponent implements OnInit {
-    ngOnInit(): void {}
+  ngOnInit() {}
 }
