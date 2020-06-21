10e47503205484cf088b046428de6343e3b4ed66
Wojciech Gruszka <wojciech@gruszka.eu>
2020-06-21 23:08:16 +0200

    Formatting

--- apsi-enrollment-fe/karma.conf.js
+++ apsi-enrollment-fe/karma.conf.js
@@ -30,8 +30,8 @@ module.exports = function(config) {
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
-      }
+        flags: ['--no-sandbox'],
+      },
     },
     singleRun: false,
     restartOnFileChange: true,
