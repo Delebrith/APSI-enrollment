3c57f551c7b802f83bc16b55e0b224903332b296
Wojciech Gruszka <wojciech@gruszka.eu>
2020-06-21 18:04:33 +0200

    Removed Home page

--- apsi-enrollment-fe/src/app/modules/main/main.module.ts
+++ apsi-enrollment-fe/src/app/modules/main/main.module.ts
@@ -6,23 +6,21 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { AppLevelAlertComponent } from './components/app-level-alert/app-level-alert.component';
 import { AttendancePdfComponent } from './components/attendance-pdf/attendance-pdf.component';
 import { EnrollmentsPdfComponent } from './components/enrollments-pdf/enrollments-pdf.component';
+import { QrCodeModalComponent } from './components/qr-code-modal/qr-code-modal.component';
 import { MainComponent } from './main.component';
 import { MainRoutingModule } from './main.routing';
 import { AllEventsComponent } from './pages/all-events/all-events.component';
 import { AllPaymentsComponent } from './pages/all-payments/all-payments.component';
 import { EventDetailComponent } from './pages/event-detail/event-detail.component';
-import { HomeComponent } from './pages/home/home.component';
 import { MyAttendancesComponent } from './pages/my-attendances/my-attendances.component';
 import { MyEnrolledEventsComponent } from './pages/my-enrolled-events/my-enrolled-events.component';
 import { MyEventsComponent } from './pages/my-events/my-events.component';
 import { MyOrganizerEventsComponent } from './pages/my-organizer-events/my-organizer-events.component';
 import { MySpeakerEventsComponent } from './pages/my-speaker-events/my-speaker-events.component';
 import { NewEventComponent } from './pages/new-event/new-event.component';
-import { QrCodeModalComponent } from './components/qr-code-modal/qr-code-modal.component';
 
 @NgModule({
   declarations: [
-    HomeComponent,
     MainComponent,
     AllEventsComponent,
     MyEventsComponent,
