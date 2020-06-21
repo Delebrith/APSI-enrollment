18fed6e25e76ab63b358cd745936fa78775f0f1f
Wojciech Gruszka <wojciech@gruszka.eu>
2020-06-21 21:54:08 +0200

    Error fix

--- apsi-enrollment-fe/src/app/modules/main/guards/event-attendance-list.guard.ts
+++ apsi-enrollment-fe/src/app/modules/main/guards/event-attendance-list.guard.ts
@@ -1,21 +1,25 @@
 import { Injectable } from '@angular/core';
-import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
-import {combineLatest, Observable} from 'rxjs';
-import { map} from 'rxjs/operators';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { combineLatest, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { CurrentUserService } from '../../../core/auth/current-user.service';
-import { EventService}  from '../services/event/event.service';
+import { EventService } from '../services/event/event.service';
 
 @Injectable()
 export class EventGuard implements CanActivate {
-  constructor(private eventService: EventService, private currentUserService: CurrentUserService, private router: Router) {}
+  constructor(
+    private eventService: EventService,
+    private currentUserService: CurrentUserService,
+    private router: Router
+  ) {}
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-    const event$ = this.eventService.getEventById(next.params.id)
+    const event$ = this.eventService.getEventById(next.params.id);
     const currentUser$ = this.currentUserService.currentUser$;
     return combineLatest([event$, currentUser$]).pipe(
       map(([event, currentUser]) => {
-        return event.organizer.id == currentUser.id;
+        return event.organizer.id === currentUser.id;
       })
-    )
+    );
   }
 }
